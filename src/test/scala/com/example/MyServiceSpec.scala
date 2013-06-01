06b3b423f2cee0e4d94175b0568308a0ecdb636a
Carlos Quiroz <cquiroz@ketalo.com>
2013-05-31 23:28:38 -0400

    Updated tests to match the new test route

--- src/test/scala/com/example/MyServiceSpec.scala
+++ src/test/scala/com/example/MyServiceSpec.scala
@@ -5,26 +5,25 @@ import spray.testkit.Specs2RouteTest
 import spray.http._
 import StatusCodes._
 
-
 class MyServiceSpec extends Specification with Specs2RouteTest with MyService {
   def actorRefFactory = system
   
   "MyService" should {
 
-    "return a greeting for GET requests to the root path" in {
-      Get() ~> myRoute ~> check {
-        entityAs[String] must contain("Say hello")
-      }
-    }
-
-    "leave GET requests to other paths unhandled" in {
-      Get("/kermit") ~> myRoute ~> check {
+    "return a not found for the root path" in {
+      Get() ~> testRoute ~> check {
         handled must beFalse
       }
     }
 
+    "leave GET requests to other paths unhandled" in {
+      Get("/test") ~> testRoute ~> check {
+        entityAs[String] must contain(""""name": "abc"""")
+      }
+    }
+
     "return a MethodNotAllowed error for PUT requests to the root path" in {
-      Put() ~> sealRoute(myRoute) ~> check {
+      Put("/test") ~> sealRoute(testRoute) ~> check {
         status === MethodNotAllowed
         entityAs[String] === "HTTP method not allowed, supported methods: GET"
       }
